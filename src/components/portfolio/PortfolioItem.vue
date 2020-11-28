120efa65de0b04f5a17d09f972b1e98004889e6a
thebigdavec <daverbcollison@gmail.com>
2020-11-28 20:18:39 +0000

    Update PortfolioItem.vue
    Move modal close button to center-justification to be more consistent with mobil modal menu close button

--- src/components/portfolio/PortfolioItem.vue
+++ src/components/portfolio/PortfolioItem.vue
@@ -99,7 +99,9 @@ li {
     position: absolute;
     cursor: pointer;
     top: 1.5rem;
-    right: 2rem;
+    left: 50%;
+    transform: translateX(-50%);
+    color: $clr-primary-dark;
     font-size: 1.25rem;
     transition: color 250ms ease;
     &:hover {
